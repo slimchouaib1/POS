68fbdc858aa3ded60454e50e3cf11313264ade88
Slim Chouaib <153125049+slimchouaib1@users.noreply.github.com>
2026-07-31 22:13:42 +0100

    UPDATE

---
+++ postgres_backup.sql
@@ -0,0 +1,26 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict bs38DQLoV8MK6arn5mh4cgcR3Xoc4NbAUswweJLwzKqhoyw3eunnuxo3Ul0waQF
+
+-- Dumped from database version 16.14
+-- Dumped by pg_dump version 16.14
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict bs38DQLoV8MK6arn5mh4cgcR3Xoc4NbAUswweJLwzKqhoyw3eunnuxo3Ul0waQF
+
